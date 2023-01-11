2372a9cade9982728158679436e44eef49a81e94
Denis Ngugi Gathondu <denis.gathondu@yahoo.com>
2023-01-12 01:18:36 +0300

    minor changes
    Signed-off-by: Denis Ngugi Gathondu <denis.gathondu@yahoo.com>

--- apps/api/app.ts
+++ apps/api/app.ts
@@ -1,6 +1,6 @@
 /* eslint-disable import/first */
-import * as dotenv from "dotenv";
-dotenv.config({ path: "../../.env.local" });
+import * as dotenv from 'dotenv'
+dotenv.config({ path: '../../.env.local' })
 
 import express, {
   Application,
@@ -9,65 +9,70 @@ import express, {
   NextFunction,
   json,
   urlencoded,
-} from "express";
+} from 'express'
 
-import { createHandler } from "graphql-http/lib/use/express";
-import cors, { CorsOptions } from "cors";
+import { createHandler } from 'graphql-http/lib/use/express'
+import cors, { CorsOptions } from 'cors'
+import helmet from 'helmet'
+import morgan from 'morgan'
 
-import { schema, db } from "./utils/index.js";
+import { schema, db } from './utils/index.js'
 
-const port: number | string = process.env.SERVER_PORT || 5000;
-const dbUri: string = process.env.DB_STRING || "";
-const app: Application = express();
-const vercel_hosts = `https://${process.env.VERCEL_URL},http://${process.env.VERCEL_URL},${process.env.BASE_URL}`;
-const domains: string = process.env.ALLOWED_ORIGINS || vercel_hosts || "";
-const whitelist: string[] = domains.split(",").map((domain) => domain.trim());
+const port: number | string = process.env.SERVER_PORT || 5000
+const dbUri: string = process.env.DB_STRING || ''
+const app: Application = express()
+const vercel_hosts = `https://${process.env.VERCEL_URL},http://${process.env.VERCEL_URL},${process.env.BASE_URL}`
+const domains: string = process.env.ALLOWED_ORIGINS || vercel_hosts || ''
+const whitelist: string[] = domains.split(',').map(domain => domain.trim())
 
 const corsOptions = {
   origin: function (
     origin: string,
-    callback: (arg0?: null | Error, arg1?: boolean) => void
+    callback: (arg0?: null | Error, arg1?: boolean) => void,
   ): void {
     if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
+      callback(null, true)
     } else {
-      callback(new Error("Not allowed by CORS"));
+      callback(new Error('Not allowed by CORS'))
     }
   },
   credentials: true,
-};
-app.use(cors(corsOptions as CorsOptions));
+}
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }))
+app.use(morgan('common'))
+app.use(cors(corsOptions as CorsOptions))
 app.use((req: Request, res: Response, next: NextFunction) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-  );
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization',
+  )
   res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  );
-  res.setHeader("Content-Type", "text/html");
-  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
-  next();
-});
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+  )
+  res.setHeader('Content-Type', 'text/html')
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
+  next()
+})
 
-app.use(json());
-app.use(urlencoded({ extended: true }));
+app.use(json())
+app.use(urlencoded({ extended: true }))
 
 await db.connectToServer(dbUri, function (err: string): void {
   if (err) {
-    console.log(`MONGODB ERROR: ${err}`);
-    process.exit(1);
+    console.log(`MONGODB ERROR: ${err}`)
+    process.exit(1)
   }
-});
+})
 
-app.all("/api", createHandler({ schema }));
+app.all('/api', createHandler({ schema }))
 
 if (!process.env.VERCEL) {
   app.listen(port, async () => {
-    console.log(`Server runnning on port: ${port}`);
-  });
+    console.log(`Server runnning on port: ${port}`)
+  })
 }
 
-export default app;
+export default app
--- packages/utils/graphql/fetcher.ts
+++ packages/utils/graphql/fetcher.ts
@@ -1,28 +1,28 @@
 export const fetchData = <TData, TVariables>(
   query: string,
   variables?: TVariables,
-  options?: RequestInit["headers"]
+  options?: RequestInit['headers'],
 ): (() => Promise<TData>) => {
   return async () => {
     const res = await fetch(`${process.env.REACT_APP_API_URL}`, {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
         ...(options ?? {}),
       },
       body: JSON.stringify({
         query,
         variables,
       }),
-    });
+    })
 
-    const json = await res.json();
+    const json = await res.json()
 
     if (json.errors) {
-      const { message } = json.errors[0] || "Error..";
-      throw new Error(message);
+      const { message } = json.errors[0] || 'Error..'
+      throw new Error(message)
     }
 
-    return json.data;
-  };
-};
+    return json.data
+  }
+}
