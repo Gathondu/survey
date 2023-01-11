6af4d0e2d5f672fe76a4283d7b1bec41f3af7721
Denis Ngugi Gathondu <denis.gathondu@yahoo.com>
2023-01-12 01:30:57 +0300

    fix webpack dev settings
    Signed-off-by: Denis Ngugi Gathondu <denis.gathondu@yahoo.com>

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,10 +1,18 @@
 /** @type {import('next').NextConfig} */
+const { parsed: surveyEnvs } = require('dotenv').config({
+  path: '../../.env.local',
+})
+const webpack = require('webpack')
 const nextConfig = {
   experimental: {
     transpilePackages: ['ui', 'utils'],
   },
   reactStrictMode: true,
   swcMinify: true,
+  webpack: config => {
+    config.plugins.push(new webpack.EnvironmentPlugin(surveyEnvs))
+    return config
+  },
 }
 
 module.exports = nextConfig
