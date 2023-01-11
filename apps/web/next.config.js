b4495ded589f1f0e518d6e74c1046918f614fb6e
Denis Ngugi Gathondu <denis.gathondu@yahoo.com>
2023-01-11 18:11:39 +0300

    remove webpack configs
    Signed-off-by: Denis Ngugi Gathondu <denis.gathondu@yahoo.com>

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,18 +1,10 @@
 /** @type {import('next').NextConfig} */
-const { parsed: surveyEnvs } = require('dotenv').config({
-  path: '../../.env.local' || '../../.env',
-})
-const webpack = require('webpack')
 const nextConfig = {
   experimental: {
     transpilePackages: ['ui', 'utils'],
   },
   reactStrictMode: true,
   swcMinify: true,
-  webpack: config => {
-    config.plugins.push(new webpack.EnvironmentPlugin(surveyEnvs))
-    return config
-  },
 }
 
 module.exports = nextConfig
