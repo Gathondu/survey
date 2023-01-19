9078a1b9131f67b007f734d66e643a60d243f69e
Denis Ngugi Gathondu <denis.gathondu@yahoo.com>
2023-01-20 01:26:11 +0300

    read environment variables from .env file
    Signed-off-by: Denis Ngugi Gathondu <denis.gathondu@yahoo.com>

--- apps/api/app.ts
+++ apps/api/app.ts
@@ -1,6 +1,6 @@
 /* eslint-disable import/first */
 import * as dotenv from 'dotenv'
-dotenv.config({ path: '../../.env.local' })
+dotenv.config({ path: '../../.env' })
 
 import express, {
   Application,
--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const { parsed: surveyEnvs } = require('dotenv').config({
-  path: '../../.env.local' || '../../.env',
+  path: '../../.env',
 })
 const webpack = require('webpack')
 const nextConfig = {
